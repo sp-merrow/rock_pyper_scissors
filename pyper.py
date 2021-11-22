82abbe8c586c0fd2038f745910a62042696f2979
Spencer Merrow <smerrow@disroot.org>
2021-11-22 14:51:49 -0500

    work in progress

--- pyper.py
+++ pyper.py
@@ -1,1 +1,12 @@
 from random import randint
+asciiArt = [[], [], []]
+
+with open('ascii_assets.txt', 'r') as raw:
+    count = -1
+    while len(asciiArt) < 3:
+        for line in raw:
+            line = line.replace('\n', '')
+            if line.isalpha():
+                count += 1
+            asciiArt[count].append(line)
+
