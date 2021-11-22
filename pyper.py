94f7f1d59b87c2c4f10d697858d1cae444d10f84
Spencer Merrow <smerrow@disroot.org>
2021-11-22 14:53:02 -0500

    work in progress

--- pyper.py
+++ pyper.py
@@ -3,10 +3,9 @@ asciiArt = [[], [], []]
 
 with open('ascii_assets.txt', 'r') as raw:
     count = -1
-    while len(asciiArt) < 3:
-        for line in raw:
-            line = line.replace('\n', '')
-            if line.isalpha():
-                count += 1
-            asciiArt[count].append(line)
+    for line in raw:
+        line = line.replace('\n', '')
+        if line.isalpha():
+            count += 1
+        asciiArt[count].append(line)
 
